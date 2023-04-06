b666130e0808efe7743d5a83ece96b0f9cc409ca
Freedisch <freeproduc@gmail.com>
2023-04-06 23:46:27 +0300

    added opportunity card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,15 @@
 import './App.css';
 import Filter from './components/Filter';
 import Navbar from './components/Navbar';
+import Opportunitycard from './components/Opportunitycard';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <div className="container-fluid">
+      <div className="container-fluid bg-slate-50">
         <Filter />
+        <Opportunitycard />
       </div>
     </div>
   );
--- frontend/src/components/Filter.tsx
+++ frontend/src/components/Filter.tsx
@@ -4,13 +4,13 @@ export default function Filter() {
   return (
     <div className="container mx-auto py-40">
       <ul className="justify-start right-0 items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-        <select className="w-56 p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none focus:border-black">
+        <select className="w-56 p-2.5 text-gray-500 bg-slate-50 border rounded-md shadow-sm outline-none focus:border-blac leading-tight">
           <option>Project manager</option>
           <option>Software engineer</option>
           <option>IT manager</option>
           <option>UI / UX designer</option>
         </select>
-        <select className="w-56 p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none  focus:border-black">
+        <select className="w-56 p-2.5 text-gray-500 bg-white/75 border rounded-md shadow-sm outline-none  focus:border-black">
           <option>Project manager</option>
           <option>Software engineer</option>
           <option>IT manager</option>
