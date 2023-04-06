13da74748580ce045ec9a49b66002091eba34bdb
Freedisch <freeproduc@gmail.com>
2023-04-06 20:28:55 +0300

    created filter components

---
+++ frontend/src/components/Filter.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function Filter() {
+  return (
+    <div className="container mx-auto py-40">
+      <ul className="justify-start right-0 items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+        <select className="w-56 p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none focus:border-black">
+          <option>Project manager</option>
+          <option>Software engineer</option>
+          <option>IT manager</option>
+          <option>UI / UX designer</option>
+        </select>
+        <select className="w-56 p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none  focus:border-black">
+          <option>Project manager</option>
+          <option>Software engineer</option>
+          <option>IT manager</option>
+          <option>UI / UX designer</option>
+        </select>
+        <select className="w-56 p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none focus:border-black">
+          <option>Project manager</option>
+          <option>Software engineer</option>
+          <option>IT manager</option>
+          <option>UI / UX designer</option>
+        </select>
+      </ul>
+    </div>
+  );
+}
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,56 @@
-import React from 'react';
+/* eslint-disable react/button-has-type */
+import React, { useState } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { GrNotification } from 'react-icons/gr';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiOutlineClose } from 'react-icons/ai';
+import { Button, buttonVariants } from './ui/Button';
 
-export default function Navbar() {
-  return <div>Navbar</div>;
+function Navbar() {
+  const [state, setState] = useState(false);
+  const navigation = [
+    { title: 'Home', path: '/' },
+    { title: 'Add Oppotunities', path: '/' },
+    { title: 'Support', path: '/' },
+    { title: 'Contact us', path: '/' },
+  ];
+  return (
+    <div className="fixed backdrop-blur-sm bg-white/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 shadow-sm flex items-center justify-between">
+      <div className="container max-w-7xl mx-auto w-full flex justify-between items-center ">
+        <img src="./src/assets/alu.png" alt="Logo" width={120} height={50} />
+
+        <div
+          className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+            state ? 'block' : 'hidden'
+          }`}
+        >
+          <ul className="justify-center items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+            {navigation.map((item, idx) => {
+              return (
+                <li key={idx}>
+                  <Button variant="ghost" size="lg">
+                    {item.title}
+                  </Button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div className="justify-end md:flex hidden">
+          <Button variant="ghost" size="lg">
+            <GrNotification />
+          </Button>
+        </div>
+
+        <div className="md:hidden">
+          <Button variant="ghost" onClick={() => setState(!state)}>
+            {state ? <AiOutlineClose /> : <GiHamburgerMenu />}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Navbar;
