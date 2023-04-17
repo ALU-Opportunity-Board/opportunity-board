ce3666c55d833ce73ed6e4bc1b6bd8cd52dc8ffd
Wubeshet Yimam <w.yimam@alustudent.com>
2023-04-17 23:35:46 +0300

    Add error handlers

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,13 +1,16 @@
 """ The server that runs that servers the API
 """
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 import config
+
 from routes.auth import AUTH_BLUEPRINT
 import os
+from flasgger import Swagger
 
 
 app = Flask(__name__)
+swagger = Swagger(app)
 # register the blueprints
 app.register_blueprint(AUTH_BLUEPRINT, url_prefix="/")
 
@@ -15,7 +18,17 @@ app.register_blueprint(AUTH_BLUEPRINT, url_prefix="/")
 CORS(app, supports_credentials=True)
 app.secret_key = config.SECRET_KEY
 # app.config['PERMANENT_SESSION_LIFETIME'] = config.PERMANENT_SESSION_LIFETIME
-# # value in seconds
+
+# 404 error handler
+@app.errorhandler(404)
+def not_found(error):
+    """Returns a JSON-formatted 404 status code response"""
+    return jsonify({"error": "Not found"}), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    """Returns a JSON-formatted 500 status code response"""
+    return jsonify({"error": "Internal server error"}), 500
 
 
 if __name__ == "__main__":
