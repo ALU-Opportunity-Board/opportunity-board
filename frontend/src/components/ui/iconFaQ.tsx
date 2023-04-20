4527efa16449685d9f2167261bf5c1baa68de120
Freedisch <freeproduc@gmail.com>
2023-04-21 00:52:00 +0300

    ui-compoanents
    Signed-off-by: Freedisch <freeproduc@gmail.com>

---
+++ frontend/src/components/ui/iconFaQ.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function IconFaQ() {
+  return (
+    <span className="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="flex-shrink-0 w-5 h-5 transition duration-300 group-open:-rotate-45"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </span>
+  );
+}
