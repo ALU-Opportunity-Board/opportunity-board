c771e10317842bd597eda3de68457868e1e0e3a9
Freedisch <freeproduc@gmail.com>
2023-04-24 22:00:00 +0300

    fixed
    Signed-off-by: Freedisch <freeproduc@gmail.com>

--- frontend/src/pages/contactPage.tsx
+++ frontend/src/pages/contactPage.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/button-has-type */
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 export default function ContactPage() {
   const contactMethods = [
@@ -21,7 +22,7 @@ export default function ContactPage() {
           />
         </svg>
       ),
-      contact: 'Support@example.com',
+      contact: 'm.batale@alustudent.com',
     },
     {
       icon: (
@@ -69,70 +70,73 @@ export default function ContactPage() {
   ];
 
   return (
-    <main className="py-14">
-      <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
-        <div className="max-w-lg mx-auto gap-12 justify-between lg:flex lg:max-w-none">
-          <div className="max-w-lg space-y-3">
-            <h3 className="text-indigo-600 font-semibold">Contact</h3>
-            <p className="text-gray-800 text-3xl font-semibold sm:text-4xl">
-              Let us know how we can help
-            </p>
-            <p>
-              We’re here to help and answer any question you might have, We look
-              forward to hearing from you! Please fill out the form, or us the
-              contact information bellow .
-            </p>
-            <div>
-              <ul className="mt-6 flex flex-wrap gap-x-10 gap-y-6 items-center">
-                {contactMethods.map((item, idx) => (
-                  <li key={idx} className="flex items-center gap-x-3">
-                    <div className="flex-none text-gray-400">{item.icon}</div>
-                    <p>{item.contact}</p>
-                  </li>
-                ))}
-              </ul>
+    <div className="bg-slate-50">
+      <Navbar />
+      <main className="py-14">
+        <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+          <div className="max-w-lg mx-auto gap-12 justify-between lg:flex lg:max-w-none">
+            <div className="max-w-lg space-y-3">
+              <h3 className="text-indigo-600 font-semibold">Contact</h3>
+              <p className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+                Let us know how we can help
+              </p>
+              <p>
+                We’re here to help and answer any question you might have, We
+                look forward to hearing from you! Please fill out the form, or
+                us the contact information bellow .
+              </p>
+              <div>
+                <ul className="mt-6 flex flex-wrap gap-x-10 gap-y-6 items-center">
+                  {contactMethods.map((item, idx) => (
+                    <li key={idx} className="flex items-center gap-x-3">
+                      <div className="flex-none text-gray-400">{item.icon}</div>
+                      <p>{item.contact}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="flex-1 mt-12 sm:max-w-lg lg:max-w-md">
+              <form onSubmit={(e) => e.preventDefault()} className="space-y-5">
+                <div>
+                  <label className="font-medium">Full name</label>
+                  <input
+                    type="text"
+                    required
+                    className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  />
+                </div>
+                <div>
+                  <label className="font-medium">Email</label>
+                  <input
+                    type="email"
+                    required
+                    className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  />
+                </div>
+                <div>
+                  <label className="font-medium">Company</label>
+                  <input
+                    type="text"
+                    required
+                    className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  />
+                </div>
+                <div>
+                  <label className="font-medium">Message</label>
+                  <textarea
+                    required
+                    className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  />
+                </div>
+                <button className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150">
+                  Submit
+                </button>
+              </form>
             </div>
           </div>
-          <div className="flex-1 mt-12 sm:max-w-lg lg:max-w-md">
-            <form onSubmit={(e) => e.preventDefault()} className="space-y-5">
-              <div>
-                <label className="font-medium">Full name</label>
-                <input
-                  type="text"
-                  required
-                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                />
-              </div>
-              <div>
-                <label className="font-medium">Email</label>
-                <input
-                  type="email"
-                  required
-                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                />
-              </div>
-              <div>
-                <label className="font-medium">Company</label>
-                <input
-                  type="text"
-                  required
-                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                />
-              </div>
-              <div>
-                <label className="font-medium">Message</label>
-                <textarea
-                  required
-                  className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                />
-              </div>
-              <button className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150">
-                Submit
-              </button>
-            </form>
-          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }
