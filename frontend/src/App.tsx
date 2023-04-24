b2e6b7f08474a38844b2c8bc67666624d90d81fe
Freedisch <freeproduc@gmail.com>
2023-04-24 21:56:40 +0300

    missing routes
    Signed-off-by: Freedisch <freeproduc@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable react/prop-types */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable @typescript-eslint/no-explicit-any */
@@ -8,16 +11,18 @@ import './App.css';
 
 import LoginPage from './pages/loginPage';
 import LandingPage from './pages/landingPage';
-import NewOpportunity from './pages/newOpportunity';
 import NotFound from './pages/404';
 import LoginSuccess from './components/LoginSuccess';
 import { setAuthUser, setIsAuthenticated } from './reducers/authReducer';
 import SupportPage from './pages/supportPage';
 import ContactPage from './pages/contactPage';
+import AddOpportunityPage from './pages/addOpportunityPage';
+import Navbar from './components/Navbar';
 
 function App() {
   // const user = useSelector((state: any) => state.App.authUser as any) as any;
   const user = true;
+
   return (
     <BrowserRouter>
       <Routes>
@@ -28,11 +33,11 @@ function App() {
         />
         <Route
           path="/new"
-          element={user ? <NewOpportunity /> : <Navigate to="" />}
+          element={user ? <AddOpportunityPage /> : <Navigate to="" />}
         />
         <Route path="/not_found" element={<NotFound />} />
         <Route path="/login_sucess" element={<LoginSuccess />} />
-        <Route path="/opportunity" element={<NewOpportunity />} />
+        <Route path="/opportunity" element={<AddOpportunityPage />} />
         <Route path="/support" element={<SupportPage />} />
         <Route path="/contactUs" element={<ContactPage />} />
       </Routes>
