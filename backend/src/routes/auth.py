14bd4c1765317275b27e3920d2160e86c851316c
Wubeshet Yimam <w.yimam@alustudent.com>
2023-04-26 22:39:06 +0300

    update google auth with jwt

--- backend/src/routes/auth.py
+++ backend/src/routes/auth.py
@@ -1,260 +1,140 @@
-""" Defines routes for authentication """
-
-""" How to Use it """
-"""
-apply the @login_is_required for users just like applied below
-    AUTH_BLUEPRINT.route("/route")
-    @login_is_required
-    def protected_area():
-"""
-
-""" when dealing with OAuth 2.0 app secret is necessary"""
-
-import datetime
-import os
-import pathlib
+import json
+from flask import Flask, Blueprint, jsonify
+from flask.wrappers import Response
+from flask.globals import request, session
 import requests
-import config
-from flask import jsonify, make_response, session, abort, redirect, request, Blueprint, url_for
+from werkzeug.exceptions import abort
+from werkzeug.utils import redirect
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
-from pip._vendor import cachecontrol
-import google.auth.transport.requests
-from datetime import datetime, timedelta
-from db import db_operations
-from models import User, db
-# from routes import login_is_required
+import os, pathlib
+import google
+import config
+from routes.register_user import register_user
+
+import jwt
+
+AUTH_BLUEPRINT = Blueprint("auth", __name__)
+
+"""this is to set environment to https because OAuth 2.0 only supports https environments"""
+
+
+"""setting path to the parent directory where the .json file from google is kept"""
 
 
 AUTH_BLUEPRINT = Blueprint("auth", __name__)
-GOOGLE_CLIENT_ID = config.GOOGLE_CLIENT_ID
-"""this is to set environment to https because OAuth 2.0 only supports https environments"""
+# bypass http
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-"""setting path to the parent directory where the .json file from google is kept"""
 client_secrets_file = os.path.join(pathlib.Path(
     __file__).parent, "../client_secret.json")
+GOOGLE_CLIENT_ID = config.GOOGLE_CLIENT_ID
+ALGORITHM = config.ALGORITHM
+BACKEND_URL=config.BACKEND_URL
+FRONTEND_URL=config.FRONTEND_URL
 
-""" 
-    Flow is OAuth 2.0 class that stores 
-    all users authorization information
-"""
+#database connection
+# connect_db()
+
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=[
         "https://www.googleapis.com/auth/userinfo.profile",
         "https://www.googleapis.com/auth/userinfo.email",
-        "openid"
+        "openid",
     ],
-    # the redirect URI route to after authorization
-    redirect_uri="http://127.0.0.1:5000/callback"
+    redirect_uri=BACKEND_URL+"/callback",
 )
 
 
-def login_is_required(function):
-    """ A decorator to check if a user is authorized or not
-
-    Args:
-        function (_type_): the function to be decorated
-    """
+# wrapper
+def login_required(function):
     def wrapper(*args, **kwargs):
-        """ The wrapper function
-
-        Returns:
-            _type_: the function that is decorated
-        """
-        if "user" not in session:
+        auth_token = request.headers.get("Authorization")
+        if auth_token is None:
+            return jsonify({"error": "Token not found"}), 401
+        encoded_jwt = auth_token.split("Bearer ")[1]
+        if encoded_jwt is None:
             return abort(401)
         else:
             return function()
-    wrapper.__name__ = function.__name__
     return wrapper
 
-def add_new_user(id_info):
-    """ Add a new user to the session database.
 
-    Args:
-        id_info (_type_): User Id information returned from google
-    """
-     # check if user is already in the database
-    # user = session.query(User).filter_by(email=id_info.get("email")).first()
-    user = db.session.query(User).filter_by(email=id_info.get("email")).first()
-    # print("user retrieved: ", user)
-    print("============I have reached here and here is user data i get=================")
-    print(user)
-    if user is None:
-        # get email from google account
-        user_email = id_info.get("email")
-        if user_email.endswith("@alustudent.com"):
-            role = "student"
-        elif user_email.endswith("@alueducation.com"):
-            role = "staff"
-        
-        # create a new user to store it in the database
-        new_user = {
-        "first_name": id_info.get("given_name"),
-        "last_name": id_info.get("family_name"),
-        "email": id_info.get("email"),
-        "picture": id_info.get("picture"),
-        "role": role
-        }
-        
-        
-        # create a new user object
-        first_time_user = User(first_name=new_user.get("first_name"),
-                               last_name=new_user.get("last_name"),
-                               email=new_user.get("email"),
-                               picture=new_user.get("picture"),
-                               role=new_user.get("role"))
-        
-        # Add the new user to the database
-        session["user"] = new_user
-        db_operations.add(first_time_user)
-        db_operations.save()
-    else:
-        # get user from the database based on cookie settings
-        print("here is the user email found from the cookie settings")
-        
-        
-        
-        # print("========cookie email=========", google_user_email)
-        # user = db.session.query(User).filter_by(email=google_user_email).first()
-        # print("User in the else of add_user func: " , user)
-        
-        existing_user = {
-         "first_name": user.first_name,
-        "last_name": user.last_name,
-        "email": user.email,
-        "picture": user.picture,
-        "role": user.role
-        }
-        session['user'] = existing_user
-        
-
-@AUTH_BLUEPRINT.route("/")
-def index():
-    """ 
-        This is to check if the user is logged in or not
-    """
-    # if user in session return logged in user
-    
-    # if "user" in session or "google_user" in request.cookies:
-    #     return "logged in"
-    
-    if "user" in session:
-        return jsonify(session["user"]), 200
-    elif "google_user" in request.cookies:
-        # get "google_user" from cookie
-        google_user_email = request.cookies["google_user"]
-        # retrieve google_user data from database based on google_user_email
-        try:
-            user = db.session.query(User).filter_by(email=google_user_email).first()
-            print("user found from db:" , user)
-            existing_user = {
-                "first_name": user.first_name,
-                "last_name": user.last_name,
-                "email": user.email,
-                "picture": user.picture,
-                "role": user.role
-                }
-            session['user'] = existing_user
-            return jsonify(session["user"]), 200
-            
-        except Exception:
-            abort(500)    
-    else:
-        return "logged out"
-
-
-""""
-    Asking the flow class for the authorization (login) url
-"""
-
-
-@AUTH_BLUEPRINT.route("/login")
-def login():
-    """ Asking the flow class for the authorization (login) url
-
-    Returns:
-        _type_: redirect to the authorization url
-    """
-    # check if the user is already logged in
-    if "user" in session:
-        return redirect(url_for("auth.index"))
-    elif "google_user" in request.cookies:
-        # get "google_user" from cookie
-        google_user_email = request.cookies["google_user"]
-        # retrieve google_user data from database based on google_user_email
-        try:
-            user = db.session.query(User).filter_by(email=google_user_email).first()
-            print("user found from db:" , user)
-            session["user"] = user
-            return jsonify(session["user"]), 200
-            
-        except Exception:
-            abort(500)
-    authorization_url, state = flow.authorization_url()
-    session["state"] = state
-    return redirect(authorization_url)
+def generate_jwt(payload):
+    encoded_jwt = jwt.encode(payload, config.APP_SECRET, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 @AUTH_BLUEPRINT.route("/callback")
 def callback():
-    """ Callback function to get the authorization code.
-    It is called after the user is authenticated
-
-    Returns:
-        _type_: _description_
-    """
     flow.fetch_token(authorization_response=request.url)
-
-    if session["state"] != request.args["state"]:
-        abort(500)
-
     credentials = flow.credentials
     request_session = requests.session()
-    cached_session = cachecontrol.CacheControl(request_session)
-    token_request = google.auth.transport.requests.Request(
-        session=cached_session)
+    token_request = google.auth.transport.requests.Request(session=request_session)
 
     id_info = id_token.verify_oauth2_token(
-        id_token=credentials._id_token,
-        request=token_request,
-        audience=config.GOOGLE_CLIENT_ID
+        id_token=credentials._id_token, request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+    session["google_id"] = id_info.get("sub")
+    
+    # removing the specific audience, as it is throwing error
+    del id_info['aud']
+    jwt_token=generate_jwt(id_info)
+    
+    # register user to the database
+    register_user(id_info)
+    
+    
+    # return redirect(f"{FRONTEND_URL}?jwt={jwt_token}")
+    return Response(
+        response=json.dumps({'JWT':jwt_token}),
+        status=200,
+        mimetype='application/json'
     )
 
-    ''' defining the results to show on the page'''
-    # session["google_id"] = id_info.get("sub")
-    # session["name"] = id_info.get("name")
-    
-    '''page to redirect to after authorization'''
-    # store the user data in the session which will be return by
-    add_new_user(id_info)
-    expires = datetime.now() + timedelta(days=30)
-    response = make_response(redirect(url_for("auth.index")))
-    print("=============SESSION info===================")
-    print("session: ",session)
-    print("type of session: ", type(session))
-    print("===================")
-    print("type of session data: ", type(session["user"]))
-    print("========================================")
-    response.set_cookie("google_user", session["user"]["email"], expires=expires)
-    return response
+
+@AUTH_BLUEPRINT.route("/auth/google")
+def login():
+    authorization_url, state = flow.authorization_url()
+    # Store the state so the callback can verify the auth server response.
+    session["state"] = state
+    #TODO: uncomment the following line
+    # return jsonify({'auth_url':authorization_url}), 200
+    return Response(
+        response=json.dumps({'auth_url':authorization_url}),
+        status=200,
+        mimetype='application/json'
+    )
 
 
 @AUTH_BLUEPRINT.route("/logout")
 def logout():
-    """ This is to clear the session and log the user out """
-    
-    # remove data from cookie
-    response = make_response(redirect(url_for("auth.index")))
-    response.delete_cookie("google_user")
+    #clear the local storage from frontend
     session.clear()
-    return response
+    return Response(
+        response=json.dumps({"message":"Logged out"}),
+        status=202,
+        mimetype='application/json'
+    )
 
-@AUTH_BLUEPRINT.route("/user-data")
-@login_is_required
-def user_data():
-    """ return the user out of the session"""
-    return jsonify(session["user"])
 
+@AUTH_BLUEPRINT.route("/home")
+@login_required
+def home_page_user():
+    encoded_jwt = request.headers.get("Authorization").split("Bearer ")[1]
+    try:
+        decoded_jwt=jwt.decode(encoded_jwt, config.APP_SECRET, algorithms=[ALGORITHM,])
+        print("===================== decoded jwt ======================")
+        print(decoded_jwt)
+    except Exception as e:
+        return Response(
+            response=json.dumps({"message":"Decoding JWT Failed", "exception":e.args}),
+            status=500,
+            mimetype='application/json'
+        )
+    return Response(
+        response=json.dumps(decoded_jwt),
+        status=200,
+        mimetype='application/json'
+    )
